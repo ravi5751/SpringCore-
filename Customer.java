9d8d9f33a25f6d9acc1d3a6d529c1fa0887345cb
Ravi RR <157389147+ravi5751@users.noreply.github.com>
2024-05-03 05:07:10 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,28 @@
+package com.pojo.Application1;
+
+public class Customer {
+
+	
+		String cust_name;
+		int cust_mobile_no;
+		public String getCust_name() {
+			return cust_name;
+		}
+		public void setCust_name(String cust_name) {
+			this.cust_name = cust_name;
+		}
+		public int getCust_mobile_no() {
+			return cust mobile no;
+		}
+		public void setCust_mobile_no(int cust_mobile_no) {
+			this.cust_mobile_no = cust_mobile_no;
+		}
+		@Override
+		public String toString() {
+			return "Customer [cust_name=" + cust_name + ", cust_mobile_no=" + cust_mobile_no + "]";
+		}
+
+}	
+		
+
+
